cc659f4379098ddbfaf486ffbffd439f71dfe904
devenvoy <iambeast0069@gmail.com>
2025-01-15 00:55:24 +0530

    deploy test2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "com.devansh"
 version = "0.0.1"
 
 application {
-    mainClass.set("dev.devansh.ApplicationKt")
+    mainClass.set("com.devansh.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
