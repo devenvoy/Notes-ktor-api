f1f7479ac5d17233d99908b6f8271231a2d5aec1
devenvoy <iambeast0069@gmail.com>
2025-01-15 00:48:28 +0530

    deploy test1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "com.devansh"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("dev.devansh.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
